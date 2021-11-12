413c7f5d5eaed7ef4262c39b75bfba94a9e7b83a
sambit <sambitskd3@gmail.com>
2021-11-12 07:28:40 +0530

    used className and added css for styling component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>Nov 12 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
