c68f5e2a642e9b9fa84c5b096f24401a157f0b98
Sambit Kumar Dalai <sambitskd3@gmail.com>
2022-01-16 18:10:13 +0530

    state integrated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,19 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
